a5eefa2ac58e48679a8716bfdd010e0c6834586c
krzyhub <krzysztof.logaciuk@gmail.com>
2015-02-11 11:54:03 +0100

    pep8
    styl z "how to python"

--- src/presence_analyzer/main.py
+++ src/presence_analyzer/main.py
@@ -7,12 +7,17 @@ from flask import Flask
 
 
 MAIN_DATA_CSV = os.path.join(
-    os.path.dirname(__file__), '..', '..', 'runtime', 'data', 'sample_data.csv'
+    os.path.dirname(__file__),
+    '..',
+    '..',
+    'runtime',
+    'data',
+    'sample_data.csv',
 )
 
 
 app = Flask(__name__)  # pylint: disable=invalid-name
 app.config.update(
-    DEBUG = True,
-    DATA_CSV = MAIN_DATA_CSV
+    DEBUG=True,
+    DATA_CSV=MAIN_DATA_CSV,
 )
