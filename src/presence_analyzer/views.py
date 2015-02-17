55b0e79eec1bb9373df64f53f7531232d781ac45
krzyhub <krzysztof.logaciuk@gmail.com>
2015-02-17 09:52:51 +0100

    bug_5559 added additional function for api for 7 days

--- src/presence_analyzer/views.py
+++ src/presence_analyzer/views.py
@@ -5,9 +5,16 @@ Defines views.
 
 import calendar
 from flask import redirect, abort
+import time
 
 from presence_analyzer.main import app
-from presence_analyzer.utils import jsonify, get_data, mean, group_by_weekday
+from presence_analyzer.utils import(
+    jsonify,
+    get_data,
+    mean,
+    group_by_weekday,
+    seconds_since_midnight
+)
 
 import logging
 log = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -73,3 +80,41 @@ def presence_weekday_view(user_id):
 
     result.insert(0, ('Weekday', 'Presence (s)'))
     return result
+
+
+@app.route('/api/v1/presence_start_end/<int:user_id>', methods=['GET'])
+@jsonify
+def presence_start_end(user_id):
+    """
+    Returns total presence time of given user grouped by weekday.
+    """
+    data = get_data()
+    if user_id not in data:
+        log.debug('User %s not found!', user_id)
+        abort(404)
+
+    weekdays = {x: {'start': [], 'end': []} for x in range(7)}
+
+    for day in data[user_id].keys():
+        weekdays[day.weekday()]['start'].append(
+            seconds_since_midnight(data[user_id][day]['start'])
+        )
+        weekdays[day.weekday()]['end'].append(
+            seconds_since_midnight(data[user_id][day]['end'])
+        )
+    for day_idx in weekdays.keys():
+        start_time_tuple = time.gmtime(mean(weekdays[day_idx]['start']))
+        end_time_tuple = time.gmtime(mean(weekdays[day_idx]['end']))
+        weekdays[day_idx]['start'] = [
+            start_time_tuple.tm_hour,
+            start_time_tuple.tm_min,
+            start_time_tuple.tm_sec,
+        ]
+
+        weekdays[day_idx]['end'] = [
+            end_time_tuple.tm_hour,
+            end_time_tuple.tm_min,
+            end_time_tuple.tm_sec,
+        ]
+
+    return weekdays
